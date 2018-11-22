3acf30b7700c8b3f2558e9700a6d9fe6ae14631a
MoniBD <45247819+MoniBD@users.noreply.github.com>
2018-11-22 04:13:02 +0200

    Add files via upload

--- client/index.js
+++ client/index.js
@@ -10,7 +10,7 @@ var modal = document.getElementById('modal');
 var name1 = document.getElementById('name');
 var name_btn = document.getElementById('name_btn');
 var error_msg = document.getElementById('error');
-var user ="";
+var user = "";
 var menu_welcome = document.getElementById('menu_welcome');
 var typing_area = document.getElementById('typing');
 
@@ -22,7 +22,7 @@ msg.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
         btn.click();
     }
-  });
+});
 
 // send msg when the user enter "Enter" - modal window
 name1.addEventListener("keyup", function(event) {
@@ -30,20 +30,19 @@ name1.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
         name_btn.click();
     }
-  });
+});
 
 // when click on the modal btn, check if name 
 // were entred, if yes - close modal, else - show
 // error msg
-name_btn.addEventListener('click',function(){
-    
-    if((name1.value).trim() === ""){
+name_btn.addEventListener('click', function() {
+
+    if ((name1.value).trim() === "") {
         error_msg.style.display = "block";
         error_msg.style.fontSize = 14;
-    }    
-    else{
+    } else {
         user = name1.value;
-        if(error_msg.style.display === "block"){
+        if (error_msg.style.display === "block") {
             error_msg.style.display = "none";
         }
         modal.style.display = "none";
@@ -57,11 +56,11 @@ name_btn.addEventListener('click',function(){
 // check if the txt box is empty, if not 
 // wend the msg to server. 
 // change btn color ad clear txt box
-btn.addEventListener('click',function(){
-    if((msg.value).trim() != ""){
+btn.addEventListener('click', function() {
+    if ((msg.value).trim() != "") {
         var time = new Date();
-        var hour = (time.getHours()<10?"0":"") + time.getHours();
-        hour =hour + ":" + (time.getMinutes()<10?"0":"") + time.getMinutes();
+        var hour = (time.getHours() < 10 ? "0" : "") + time.getHours();
+        hour = hour + ":" + (time.getMinutes() < 10 ? "0" : "") + time.getMinutes();
         socket.emit('spotim/chat', {
             avatar: "",
             username: user,
@@ -71,7 +70,7 @@ btn.addEventListener('click',function(){
             id: socket.id
         });
         msg.value = "";
-        btn.style.background = "rgb(225,225,225)";   
+        btn.style.background = "rgb(225,225,225)";
 
     }
     typing_area.innerText = "";
@@ -82,23 +81,26 @@ btn.addEventListener('click',function(){
 //when the user is typing (key up event) -
 // emit "typing" event every 0.2 sec 
 var publish = true;
-msg.addEventListener('keyup', function(){ 
-    if(publish === true){
-        socket.emit('typing',{user: user});
+msg.addEventListener('keyup', function() {
+    if (publish == true) {
+        socket.emit('typing', {
+            user: user
+        });
         publish = false;
         setTimeout(() => {
             publish = true;
         }, 200);
     }
+
 });
 
 // when txt box isn't empty - change btn color
 // to blue, else - gray
-msg.addEventListener('keyup', function(){
+msg.addEventListener('keyup', function() {
     if (msg.value != "")
-            btn.style.background = "rgb(130, 224, 255)";       
-  else
-            btn.style.background = "rgb(225,225,225)";   
+        btn.style.background = "rgb(130, 224, 255)";
+    else
+        btn.style.background = "rgb(225,225,225)";
 });
 
 // Listen for events
@@ -106,16 +108,15 @@ msg.addEventListener('keyup', function(){
 //listen for chat msges and uptade HTML
 // for the user msg - shows in diffrent color
 // from the rest of the users 
-socket.on('spotim/chat',function(data){
-    if(data.id == socket.id){ // another color
-        output.innerHTML =  output.innerHTML + 
-        " <div class='msg-box-me'> <strong>" +data.username +"</strong>: " +data.text  +
-        "<div class='time-right'>" + data.hour+"</div></div>";
-    }
-    else{
-        output.innerHTML = output.innerHTML + 
-    "<div class='msg-box'> <strong>" +data.username +"</strong>: " +data.text + 
-    "<div class='time-right'>" + data.hour+"</div></div>";
+socket.on('spotim/chat', function(data) {
+    if (data.id == socket.id) { // another color
+        output.innerHTML = output.innerHTML +
+            " <div class='msg-box-me'> <strong>" + data.username + "</strong>: " + data.text +
+            "<div class='time-right'>" + data.hour + "</div></div>";
+    } else {
+        output.innerHTML = output.innerHTML +
+            "<div class='msg-box'> <strong>" + data.username + "</strong>: " + data.text +
+            "<div class='time-right'>" + data.hour + "</div></div>";
     }
 });
 
@@ -123,9 +124,8 @@ socket.on('spotim/chat',function(data){
 // update the HTML
 // clear it every 0.9 sec
 var clearTimerId;
-socket.on('typing',function(data){
-    typing_area.innerHTML = ""+ data.user +" is typing...";
-
+socket.on('typing', function(data) {
+    typing_area.innerHTML = "" + data.user + " is typing...";
     //Timer for stop typing - every 0.9 sec
     clearTimeout(clearTimerId);
     clearTimerId = setTimeout(() => {
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 // create server 
-var server = app.listen(4000,function(){
+var server = app.listen(4000, function() {
     console.log("listening to port 4000");
 });
 // create io connection
@@ -13,22 +13,22 @@ app.use(express.static('client'));
 
 // listen for users connetcions
 // socket - the connection between the user and the server
-io.on('connection',function(socket){
+io.on('connection', function(socket) {
     console.log("We have a new connected friend");
-    
+
     // listening for chat events and emit them to 
     // all the users 
-    socket.on('spotim/chat', function(data){
+    socket.on('spotim/chat', function(data) {
         io.sockets.emit('spotim/chat', data);
     });
     //listen for typing events and emit them to 
     // the relevant users 
-    socket.on('typing',function(data){
-        socket.broadcast.emit('typing',data)
+    socket.on('typing', function(data) {
+        socket.broadcast.emit('typing', data)
     });
-    
+
     // listen for disconect events
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function() {
         console.log("One friend leave us");
     })
 });
